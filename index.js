9a6d477dad2e9dbe006c09b2c0515e4e6512bcd6
Johnny Austin <johnny@johnnyray.me>
2016-05-03 10:32:55 -0400

    formatting

--- index.js
+++ index.js
@@ -2,85 +2,85 @@ require('dotenv').config();
 const request = require('request');
 
 const ParseObjectType = function(objName) {
-	const apiHost = process.env.API_HOST || 'http://localhost:4321';
-	this.customRequest = request.defaults({
-  	headers: {
-  		'X-Parse-Application-Id': process.env.APP_ID,
-  		'X-Parse-REST-API-Key': process.env.API_KEY,
-  		'Content-Type': 'application/json'
-  	}
-	});
-	this.objName = objName;
-	this.baseUrl = `${apiHost}/parse/classes/${objName}`;
+  const apiHost = process.env.API_HOST || 'http://localhost:4321';
+  this.customRequest = request.defaults({
+    headers: {
+    'X-Parse-Application-Id': process.env.APP_ID,
+    'X-Parse-REST-API-Key': process.env.API_KEY,
+    'Content-Type': 'application/json'
+    }
+  });
+  this.objName = objName;
+  this.baseUrl = `${apiHost}/parse/classes/${objName}`;
 };
 
 ParseObjectType.prototype.getAll = function(onGet) {
-	this.customRequest(this.baseUrl, (err, httpR, body) => {
-		if (err) {
-			onGet(err);
-		} else {
-			parseResponse(body, onGet);
-		}
-	});
+  this.customRequest(this.baseUrl, (err, httpR, body) => {
+    if (err) {
+      onGet(err);
+    } else {
+      parseResponse(body, onGet);
+    }
+  });
 };
 
 ParseObjectType.prototype.get = function(id, onGet) {
-	var requestUrl = `${this.baseUrl}/${id}`;
-	this.customRequest(requestUrl, (err, httpR, body) => {
-		if (err) {
-			onGet(err);
-		} else {
-			parseResponse(body, onGet);
-		}
-	});
+  const requestUrl = `${this.baseUrl}/${id}`;
+  this.customRequest(requestUrl, (err, httpR, body) => {
+    if (err) {
+      onGet(err);
+    } else {
+      parseResponse(body, onGet);
+    }
+  });
 };
 
 ParseObjectType.prototype.create = function(props, onCreate) {
-	this.customRequest.post({
-		url: this.baseUrl,
-		form: props
-	},
-	(err, httpR, body) => {
-		if (err) {
-			onCreate(err);
-		} else {
-			parseResponse(body, onCreate);
-		}
-	});
+  this.customRequest.post({
+    url: this.baseUrl,
+    form: props
+  },
+  (err, httpR, body) => {
+    if (err) {
+      onCreate(err);
+    } else {
+      parseResponse(body, onCreate);
+    }
+  });
 };
 
 ParseObjectType.prototype.update = function(objId, props, onUpdate) {
-	this.customRequest.put({
-		url: `${this.baseUrl}/${objId}`,
-		form: props
-	},
-	(err, httpR, body) => {
-		if (err) {
-			onUpdate(err);
-		} else {
-			parseResponse(body, onUpdate);
-		}
-	});
+  this.customRequest.put({
+    url: `${this.baseUrl}/${objId}`,
+    form: props
+  },
+  (err, httpR, body) => {
+    if (err) {
+      onUpdate(err);
+    } else {
+      parseResponse(body, onUpdate);
+    }
+  });
 };
 
 ParseObjectType.prototype.remove = function(objId, onRemove) {
-	this.customRequest.delete(this.baseUrl + '/' + objId, (err) => {
-		if (err) {
-			onRemove(err);
-		} else {
-			onRemove(null, { message: `Successfully deleted object ${objId}` });
-		}
-	});
+  this.customRequest.delete(this.baseUrl + '/' + objId, (err) => {
+    if (err) {
+      onRemove(err);
+    } else {
+      onRemove(null, { message: `Successfully deleted object ${objId}` });
+    }
+  });
 };
 
 function parseResponse(body, callback) {
-	try {
-		const data = JSON.parse(body);
-		callback(null, data);
-	} catch(e) {
-		console.error(e);
-		callback(Error(`An error occured while parsing response: ${e.message}`));
-	}
+  try {
+    const data = JSON.parse(body);
+    callback(null, data);
+  } catch(e) {
+    console.error(e);
+    callback(Error(`An error occured while parsing response: ${e.message}`));
+  }
 }
 
 module.exports = function(objType, local) {
